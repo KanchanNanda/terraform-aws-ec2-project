e398e3632090036c3e17eeeafbc1054a572f5014
KanchanNanda <nandakanchan400@gmail.com>
2026-07-30 13:50:25 +0530

    Initial Terraform project setup with AWS provider

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+    description = "AWS Region"
+    type       = string
+    default = "ap-south-1"
+}
+
+variable "Terraform-aws-ec2-project" {
+    description = "Terraform-aws-ec2-project"
+    type = string 
+    default = "terraform-devops-project"
+}
